5a613e0c5b8206687a81111a5f4793743adc492c
GothamCityEmployee <corycarvalho13@gmail.com>
2026-04-01 09:58:18 -0400

    Fix content-type for Apple Pay verification

--- api/apple-pay-verification.js
+++ api/apple-pay-verification.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = function handler(req, res) {
   const filePath = path.join(process.cwd(), '.well-known', 'apple-developer-merchantid-domain-association');
   const content = fs.readFileSync(filePath, 'utf8');
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
   res.setHeader('Content-Encoding', 'identity');
   res.setHeader('Cache-Control', 'no-store');
   res.status(200).send(content);
